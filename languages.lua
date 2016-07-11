89652a43919ce0c26daa4c2334dad2bc67015b20
ridrogo <ridrogo@gmail.com>
2016-07-11 16:11:03 -0400

    Update languages.lua

--- languages.lua
+++ languages.lua
@@ -152,7 +152,7 @@ return {
                     ..'`/help` : Ver este mensaje.',
 		           -- ..'\n\nSi te gusta este bot, por favor deja tu voto [aqui](https://telegram.me/storebot?start=groupbutler_bot)',
 		    private = '¡Hola *&&&1*!\n'
-                   ..'Soy Garp Moderador Bot, un _bot moderador_ para tus grupos.\n'
+                   ..'Soy Moderador Bot, un _bot moderador_ para tus grupos.\n'
                    ..'\n*¿Como puedo ayudarte?*\n'
                    ..'\nPuedo banear, expulsar, advertir a cualquier usuario que tú desees.\n'
                    ..'Solo necesito que me des administración y mi trabajo empieza!\n'
