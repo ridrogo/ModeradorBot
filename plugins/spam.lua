6cc5fbc2d308fdd3ffdf848e02b116a5b7b9debc
ridrogo <ridrogo@gmail.com>
2016-08-08 21:23:05 -0400

    pequeño fix al plugin anti-spam

--- plugins/spam.lua
+++ plugins/spam.lua
@@ -1,20 +1,20 @@
 local action = function(msg, blocks, ln)
 
- 			local is_normal_group = false
-	   		if msg.chat.type == 'group' then is_normal_group = true end
-	
+	if (msg.chat.type == 'private') and not is_mod(msg) then return end
+
 	local text
+
 if blocks[1] == 'antispam' then
 		if blocks[2]:match('^(on)$') and blocks[2]:match('^(off)$') then
 			api.sendMessage(msg.chat.id, 'Estados disponibles: on/off')
 			return
 		end
 		local status = blocks[2]
-		local current = db:hget('bot:general', 'antispam')
+		local current = db:hget('chat:'..msg.chat.id..':settings', 'antispam')
 		if current == status then
 			api.sendMessage(msg.chat.id, 'Modo Anti-Spam *ya está '..status..'*', true)
 		else 
-			db:hset('bot:general', 'antispam', status)
+			db:hset('chat:'..msg.chat.id..':settings', 'antispam', status)
 			api.sendMessage(msg.chat.id, 'Modo Anti-Spam: *'..status..'*', true)
 		end
 	end
